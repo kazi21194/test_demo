2df768b80f214f64ac34dcac20b1dd9f32f06476
kazi21194 <kazi21194@iiitd.ac.in>
2021-08-19 12:43:29 +0530

    included length functionality

--- int.py
+++ int.py
@@ -1,3 +1,9 @@
-from decimal import Decimal as D
-from math import pi
-print(D("pi"))
+def length(list1):
+    c = 0
+    for x in list1:
+        c += 1
+    return c
+
+print("------------------------------")
+print(length([1,25,'kazi',45,'lol',True,[]]))
+print("------------------------------")
